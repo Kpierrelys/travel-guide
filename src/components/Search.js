53bb533ad05b5b59bb100151adf23f367858daad
Kerry <kpierrelys.kp@gmail.com>
2021-06-18 21:48:32 -0400

    heading adjustment

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,7 +19,7 @@ const Search = ({ inputValue }) => {
 
     return (
         <form onSubmit={handleSubmit} className='search-engine'>
-            <h3>Where Do You Want to go?</h3>
+            <h3>See venues in different cities!</h3>
             <input type="text" name="search" placeholder='Search city..'  autoComplete='off' value={location}
           onChange={handleChange}/>
           <button><h4>Search</h4></button>
